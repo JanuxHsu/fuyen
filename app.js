7f456605cb67319630abfddbcad51a8c62bb7bcc
JanuxHsu <jackyshu1107@gmail.com>
2015-12-03 17:47:32 +0800

    23

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-PythonShell.run('generator.py', function (err) {
+PythonShell.run('py/generator.py', function (err) {
   if (err) throw err;
   console.log('finished');
 });
